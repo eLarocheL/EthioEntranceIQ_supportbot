517eae77e99937df358900e904a94e4a01ad29b2
Elilo Arja Ochono <babarobasa@gmail.com>
2026-07-23 13:39:48 +0300

    Update support.py

--- support.py
+++ support.py
@@ -27,7 +27,7 @@ def home():
 # ==========================================
 def get_main_keyboard():
     markup = InlineKeyboardMarkup()
-    markup.row(InlineKeyboardButton("🎓 Request Personal Tutor", callback_data="menu_tutor"))
+    markup.row(InlineKeyboardButton("🎓 የግል አስጠኚ እፈልጋለሁ", callback_data="menu_tutor"))
     markup.row(InlineKeyboardButton("💬 Talk to Admin", callback_data="menu_human"))
     markup.row(
         InlineKeyboardButton("⚡ Quick FAQs", callback_data="menu_faq"),
